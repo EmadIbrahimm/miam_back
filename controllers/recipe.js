8122981661fafde0758b6f6091059b47e253a45c
DDA209 <didier.pascarel1@gmail.com>
2020-10-08 18:34:20 +0200

    evening 08/10/2020 Didier

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const RecipeModel = require('../models/recipe');
+const ListIngredient = require('../models/listIngredient');
 console.log('RecipeModel', RecipeModel);
  
 // router.post("/", (req, res) => {   
@@ -37,7 +38,52 @@ console.log('RecipeModel', RecipeModel);
   
     
 // });
+router.get('/users/:id', (req, res) => {
+  console.log('GET /recipe/users/:id');
+  console.log('GET /recipe/users/:id req.params.name', req.params.id);
+  const user = req.params.id;
+  let userIngredients = [];
+  // let recipe = '';
 
+  console.log('GET /recipe/users/:id user', user);
+
+  ListIngredient.find({user})
+  .populate('ingredient')
+  .exec((err, listIngredient) => {
+    if (err) {
+      console.log('err', err)
+      return;
+    };
+    userIngredients = listIngredient.map( (el) => {
+      return {
+        _id: el.ingredient._id,
+        quantity: el.quantity
+      };
+    });
+
+    console.log('GET /recipe/users/:id userIngredients', userIngredients);
+    RecipeModel.find({}, (err, recipes)=> {
+
+      // console.log('GET /recipe/users/:id recipes', recipes);
+
+      if (err) {
+        console.log('err', err)
+        return;
+      };
+
+      recipes.forEach( recipe => {
+
+        canMakeRecipe (userIngredients, recipe)
+
+        return;
+      });
+
+      // recipe = recipes;
+      // console.log('GET /recipe/users/:id recipe', recipe);
+    })
+  });
+
+});
 
 router.get('/:id', (req, res) => {
   RecipeModel.find({}, (err, recipes)=> {
@@ -56,5 +102,28 @@ router.get('/:id', (req, res) => {
 });
 
 
+function canMakeRecipe ( userIngredients, recipe ) {
+  console.log('#canMakeRecipe');
+  console.log('#canMakeRecipe recipe.ingredients', recipe.ingredients);
+  console.log('#canMakeRecipe userIngredients', userIngredients);
+
+  recipe.ingredients.forEach( recipeIngredient => {
+    userIngredients.forEach( userIngredient => {
+      console.log('#canMakeRecipe recipeIngredient.isMandatory', recipeIngredient.isMandatory);
+      if ( recipeIngredient.isMandatory === false ){
+        console.log('#canMakeRecipe ingredient not mandatory');
+        return;
+      }
+      // if ( 
+      //   recipeIngredient._id === userIngredient._id && 
+      //   recipeIngredient.quantity >= userIngredient.quantity 
+      // ){
+      //   return true;
+      // };
+      return false;
+    })
+  })
+
+}
 
 module.exports = router;
