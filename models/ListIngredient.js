07896434cd8bc2a0d303c93ddf9af43678ae7a55
DDA209 <didier.pascarel1@gmail.com>
2020-10-09 16:09:55 +0200

    refresh  09/10/2020 Didier

--- models/ListIngredient.js
+++ models/ListIngredient.js
@@ -20,22 +20,6 @@ const listIngredientSchema = new mongoose.Schema({
 });
 
 
-<<<<<<< HEAD
-// ListIngredient sans s 
-const listIngredient = new ListIngredient({
-    quantity :'onions',
-    unity :'2 Kg',
-    
-  });
-  
-  listIngredient.save((err,ListIngredientDB)=>{
-    console.log('err',err);
-   console.log('ListIngredientDB',ListIngredientDB)
-
-});
-  
-=======
 const ListIngredient = mongoose.model('ListIngredient',listIngredientSchema);
->>>>>>> 3a985e4978a364033148d3eb83543956f2ea6687
 
 module.exports = ListIngredient;
